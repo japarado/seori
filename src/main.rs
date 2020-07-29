4ccb4dab63dcc1e5ad57ffe8995cedb83639dd78
Justin Parado <justin.parado.personal@gmail.com>
2020-07-29 12:23:59 +0800

    Application-wide database conector

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
 #[macro_use]
 extern crate diesel;
 
+use std::sync::Mutex;
+
 use actix_web::get;
-use actix_web::{App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use std::io;
 
 mod database;
 
+#[derive(Clone)]
+pub struct ApplicationData {
+    conn_pool: database::Pool
+}
+
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
+
+    let application_data = ApplicationData {
+        conn_pool: database::create_pool()
+    };
+
+    let data = web::Data::new(Mutex::new(application_data));
+
+    HttpServer::new(|| 
+        App::new()
+        .app_data(data.clone())
+        .service(index)
+        )
         .bind("localhost:8080")?
         .run()
         .await
